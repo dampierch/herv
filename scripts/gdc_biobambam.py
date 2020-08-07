9474687dce5ddf735d28a25a0039801918a5e66a
dampierch <odampso@yahoo.com>
2020-08-07 14:39:45 -0400

    add convert_tcga rule

---
+++ scripts/gdc_biobambam.py
@@ -0,0 +1,216 @@
+'''
+-- module built for python3
+-- identify bam files for conversion to fastq
+-- submit jobs for biobambam conversion on rivanna
+-- requires config.sh to set environmental variables
+-- for use in snakemake workflow
+-- usage: snakemake convert_tcga
+'''
+
+
+import os
+import glob
+import subprocess
+import argparse
+import math
+
+
+modname = 'gdc_biobambam'
+
+
+def run_mod(**par):
+    '''
+    function to execute submission of sbatch job associated with this module
+    '''
+    os.chdir(par['command_dir'])
+    fp = par['out_path']
+    fn = '.'.join([modname, 'idx'])
+    with open(fp + fn, 'w') as outfile:
+        for k in par['file_dict']:
+            outfile.write('%s::%s\n' % (k, par['file_dict'][k]))
+    output = '_'.join([modname, '%a.out'])
+    shell_args = ' '.join(
+            [
+                par['in_path'], par['ext'], par['read_format'],
+                str(par['batch_size']), par['out_path']
+            ]
+        )
+    cmd = ' '.join(
+            [
+                'sbatch', '--output=' + output,
+                '--array=1-' + str(par['array_max']),
+                '.'.join([modname,'sh']), shell_args
+            ]
+        )
+    if par['live'] == True:
+        subprocess.call(cmd, shell=True)
+    else:
+        print(cmd)
+
+
+def get_ids(ann, read_format):
+    '''
+    load selected file_ids and subject_ids into dict for processing
+    '''
+    ids = {}
+    line_num = 0
+    with open(ann, 'r') as in_file:
+        for in_line in in_file:
+            if line_num == 0:
+                fields = in_line.strip('\n').split('\t')
+            else:
+                d = dict(list(zip(fields, in_line.strip('\n').split('\t'))))
+                if d['read_format'] == read_format:
+                    ids[d['subject_id']] = d['file_id']
+            line_num = line_num + 1
+    return ids
+
+
+def parse_filesets(
+        ids, in_path, infile_ext, read_format, out_path, outfile_ext,
+        batch_size
+    ):
+    '''
+    determine which files to process and how many batches are required
+    -- return files to process in dict with subject_id as key
+    '''
+    fs_in = glob.glob(in_path + '*/*' + infile_ext)
+    d_in = {}
+    for f in fs_in:
+        if f[len(in_path):].split('/')[0] in ids.values():
+            d_in[f[len(in_path):].split('/')[0]] = f[len(in_path):]
+    fs_out = glob.glob(out_path + '*' + outfile_ext)
+    l_out = []
+    for f in fs_out:
+        if f[len(out_path):-len(outfile_ext)] in ids:
+            l_out.append(f[len(out_path):-len(outfile_ext)])
+    filenum = len(d_in) - len(l_out)  ## unprocessed minus processed
+    array_max = math.ceil(filenum / batch_size)
+    l_out = [d_in[ids[id]] for id in l_out]
+    fs_done = [f for f in l_out]
+    fs_todo = [d_in[f] for f in d_in if d_in[f] not in fs_done]
+    ids = {v: k for k, v in ids.items()}  ## invert ids dict to write todo list
+    d_todo = {}
+    for f in fs_todo:
+        d_todo[ids[f.split('/')[0]]] = ''.join([in_path, f])
+    return array_max, d_todo
+
+
+def parse_params(
+        command_dir, in_path, infile_ext, read_format, batch_size,
+        out_path, array_max, d_todo, run_type
+    ):
+    '''
+    set parameters
+    '''
+    params = {
+        'command_dir': command_dir,
+        'in_path': in_path,
+        'ext': infile_ext,
+        'read_format': read_format,
+        'batch_size': batch_size,
+        'out_path': out_path,
+        'array_max': array_max,
+        'file_dict': d_todo,
+        'live': run_type
+    }
+    return params
+
+
+def set_source(modname, source):
+    '''
+    set input path and file extension depending on module and source
+    '''
+    input_path_dict = {
+        'gdc_biobambam': {
+            'tcga': os.environ['download_dir']
+        }
+    }
+    input_ext_dict = {
+        'gdc_biobambam': {
+            'tcga': '.bam'
+        }
+    }
+    return input_path_dict[modname][source], input_ext_dict[modname][source]
+
+
+def set_dest(modname):
+    '''
+    set output path and file extension depending on module
+    '''
+    out_path_dict = {
+        'gdc_biobambam': os.environ['fastq_dir']
+    }
+    out_ext_dict = {
+        'gdc_biobambam': '.fastq.gz'
+    }
+    return out_path_dict[modname], out_ext_dict[modname]
+
+
+def check_outpath(out_path, live):
+    '''
+    check for presence of absence of out_path and make directory if absent
+    '''
+    l = out_path.strip('/').split('/')
+    d = ''
+    for e in l:
+        d = d + '/' + e
+        if os.path.exists(d):
+            print(d,'present')
+        else:
+            print(d,'absent')
+            print('making',d,'now')
+            if live:
+                os.mkdir(d)
+
+
+def main(args):
+    '''
+    function to organize execution of tasks across batches
+    '''
+    command_dir = os.environ['command_dir']
+    work_dir = os.environ['work_dir']
+    in_path, infile_ext = set_source(modname, 'tcga')
+    out_path, outfile_ext = set_dest(modname)
+    read_format = args.read_format
+    batch_size = int(args.batch_size)
+    run_type = args.run_type
+    ann = os.environ['ann_dir'] + 'tcga/' + 'ann_select.tsv'
+    ids = get_ids(ann, read_format)
+    check_outpath(out_path, run_type)
+    array_max, d_todo = parse_filesets(
+            ids, in_path, infile_ext, read_format, out_path, outfile_ext,
+            batch_size
+        )
+    params = parse_params(
+            command_dir, in_path, infile_ext, read_format, batch_size,
+            out_path, array_max, d_todo, run_type
+        )
+    run_mod(**params)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='biobambam')
+    parser.add_argument(
+        '--read_format', help='paired or single reads', action='store',
+        dest='read_format'
+    )
+    parser.add_argument(
+        '--batch_size', help='number of samples to process in single batch',
+        action='store', dest='batch_size'
+    )
+    parser.add_argument(
+        '--live', help='execute a live run', action='store_true',
+        dest='run_type'
+    )
+    parser.add_argument(
+        '--test', help='execute a test run', action='store_false',
+        dest='run_type'
+    )
+    parser.set_defaults(
+        read_format='paired', batch_size=10, run_type=False
+    )
+    args = parser.parse_args()
+    main(args)
+else:
+    print('functions loaded for', modname)
---
+++ scripts/gdc_biobambam.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5000
+#SBATCH --time=05:00:00
+#SBATCH --partition=standard
+#SBATCH --account=chd5n_alloc
+#SBATCH --mail-type=END
+#SBATCH --mail-user=chd5n@virginia.edu
+
+## -- requires config.sh to set environmental variables
+## -- for use in snakemake workflow
+## -- usage: snakemake convert_tcga
+
+## -- perform: 1h, 41MB for 220 bam single, 8 batches of 30, 1 cpu
+
+
+script_name=gdc_biobambam.sh
+
+
+## command-line arguments
+in_path=$1
+infile_ext=$2
+read_format=$3
+batch_size=$4
+out_path=$5
+SATID=$SLURM_ARRAY_TASK_ID
+CPUS=$SLURM_CPUS_PER_TASK
+
+
+startup_report () {
+  ## report workflow parameters
+  pwd; hostname; date
+  echo Starting $script_name ...
+  echo
+  echo Format: $read_format
+  echo Batch size: $batch_size
+  echo Input: $in_path
+  echo Output: $out_path
+  echo Array: $SATID
+  echo CPUs: $CPUS
+  echo
+}
+
+
+set_task_idx () {
+  ## single input per sample obviates need for selection of unique samples
+
+  d=$(( $batch_size * ( $SATID - 1 ) ))
+  q=$(( $batch_size * $SATID ))
+
+  if [ $SATID -eq 1 ]; then
+    cat ${out_path}${script_name%.sh}.idx | \
+      sed -e "${q}q" > ${out_path}${script_name%.sh}_idx_${SATID}.tmp
+  elif [ $SATID -gt 1 ]; then
+    cat ${out_path}${script_name%.sh}.idx | \
+      sed -e "1,${d}d;${q}q" > ${out_path}${script_name%.sh}_idx_${SATID}.tmp
+  fi
+}
+
+
+reset_path () {
+  ## purge non-default paths
+  ## add paths to required binaries
+  echo Resetting \$PATH ...
+  module list
+  echo Currently loaded modules will be purged
+  module purge
+  echo Adding biobambam2/2.0.87 to \$PATH ...
+  PATH=$PATH:~/apps/biobambam2/2.0.87-release-20180301132713/x86_64-etch-linux-gnu/bin/  ## this adds biobambam bin to PATH
+}
+
+
+shutdown_report () {
+  date
+  echo end $script_name
+  echo seff $SLURM_JOBID
+  echo sacct -o reqmem,maxrss,elapsed,alloccpus,nodelist -j $SLURM_JOBID
+}
+
+
+run_module () {
+  ## initiation workflow loop
+  for elem in $(<${out_path}${script_name%.sh}_idx_${SATID}.tmp); do  ## elem includes path to input
+    sn=`echo $elem | awk -v FS="::" '{print $1}'`
+    fn=`echo $elem | awk -v FS="::" '{print $2}'`
+    echo Starting quantification for sample $sn
+    echo '--' File name: $fn
+
+    ## tcga, so some reads are paired and others are single, length is variable
+
+    if [ $read_format == 'paired' ]; then
+      bamtofastq \
+        gz=1 \
+        level=1 \
+        filename=${fn} \
+        F=${out_path}${sn}_1.fastq.gz \
+        F2=${out_path}${sn}_2.fastq.gz
+    elif [ $read_format == 'single' ]; then
+      bamtofastq \
+        gz=1 \
+        level=1 \
+        filename=${fn} \
+        S=${out_path}${sn}.fastq.gz
+    else
+      echo Encountered a problem with $sn
+      exit
+    fi
+    echo Done with quantification for sample $sn
+  done
+  rm ${out_path}${script_name%.sh}_idx_${SATID}.tmp
+}
+
+
+main () {
+  startup_report
+  set_task_idx
+  reset_path
+  run_module
+  shutdown_report
+}
+
+
+main
