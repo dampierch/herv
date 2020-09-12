afba0bafeb90b50b31be5f219bc0c6d948733676
dampierch <odampso@yahoo.com>
2020-09-12 11:00:39 -0400

    add reporting commands

--- scripts/compress_fastq.sh
+++ scripts/compress_fastq.sh
@@ -4,15 +4,20 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=20000
-#SBATCH --time=10:00:00
+#SBATCH --mem=01000
+#SBATCH --time=08:00:00
 #SBATCH --partition=standard
 #SBATCH --account=chd5n_alloc
 #SBATCH --mail-type=END
 #SBATCH --mail-user=chd5n@virginia.edu
+
+## 8MB, 1h45m for 59 fastq samples, paired, single mix
 
 target_dir=$1
 CPUS=$SLURM_CPUS_PER_TASK
 
 cd $target_dir
 pigz --fast -p $CPUS *.fastq
+
+echo seff $SLURM_JOBID
+echo sacct -o reqmem,maxrss,elapsed,alloccpus,nodelist -j $SLURM_JOBID
